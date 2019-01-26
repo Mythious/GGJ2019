68a89e9f4420dd2afdd9fafae051b7b9a7ecd346
Ben Mullenger <ben@amull.co.uk>
2019-01-26 13:32:58 +0000

    Pop Management
    Added population manager,
    Moved food ticked to MapManager prefab, this is an abstraction of the placement manager prefab.

--- Assets/Scripts/HouseScript.cs
+++ Assets/Scripts/HouseScript.cs
@@ -13,6 +13,7 @@ public class HouseScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        var popManager = GameObject.FindGameObjectWithTag("MapManager").GetComponent<PopulationScript>();
         _buildingScript = gameObject.GetComponent<BuildingScript>();
         float angleChange = (2 * Mathf.PI) / NumPeopleToSpawn;
         for(int i = 0; i < NumPeopleToSpawn; i++)
@@ -21,6 +22,7 @@ public class HouseScript : MonoBehaviour
             Vector3 pos = new Vector3(Mathf.Cos(f), 1.5f, Mathf.Sin(f)) * _buildingScript.inRadius;
             var p = GameObject.Instantiate(PersonPrefab);
             p.transform.position = pos + transform.position;
+            popManager.AddPop(p);
         }
     }
 
